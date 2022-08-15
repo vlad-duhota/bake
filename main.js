fc94d736680784502759db5966747b4fe933dd9d
vlad-duhota <vladarduino@gmail.com>
2022-08-15 16:41:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const btnsOpen = document.querySelectorAll('.hero__item, .catalog__item');
+const popUp = document.querySelector('.pop-up');
+const bluredElements = document.querySelectorAll('.header, .main, .footer');
+const bodyElem = document.querySelector('body');
+const popUpImg = document.querySelector('.pop-up__img');
+
+const handle = function(){
+    popUp.classList.add('pop-up_active');
+    bluredElements.forEach(elem => {
+        elem.classList.add('blured');
+    });
+    bodyElem.style.overflow = 'hidden';
+    popUpImg.src = this.querySelector('img').src;
+}
+
+btnsOpen.forEach(btn => {
+    btn.addEventListener('click', handle);
+});
+
+popUp.addEventListener('click', function(e){
+    if(e.target===this){
+        popUp.classList.remove('pop-up_active');
+        bluredElements.forEach(elem => {
+            elem.classList.remove('blured');
+        });
+        bodyElem.style.overflow = 'auto';
+    }
+});
