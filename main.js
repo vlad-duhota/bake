4dcde8db4174431a65a6829508a68113bcad7cc9
vlad-duhota <vladarduino@gmail.com>
2022-08-15 16:50:00 +0300

    smooth scroll added

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+const links = document.querySelectorAll("a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+
 const btnsOpen = document.querySelectorAll('.hero__item, .catalog__item');
 const popUp = document.querySelector('.pop-up');
 const bluredElements = document.querySelectorAll('.header, .main, .footer');
