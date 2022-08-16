4152e957832d111a3c6b770644d9586940f6b40c
vlad-duhota <vladarduino@gmail.com>
2022-08-16 14:59:13 +0300

    finish

--- main.js
+++ main.js
@@ -43,3 +43,48 @@ popUp.addEventListener('click', function(e){
         bodyElem.style.overflow = 'auto';
     }
 });
+
+// menu
+const menuBtn = document.querySelector('.header__menu-btn');
+const menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('header__menu-btn_active');
+    menu.classList.toggle('menu_active');
+});
+
+// sticky header
+window.onscroll = function() {checkSticky()};
+
+// Get the header
+let header = document.querySelector('.header');
+
+// Get the offset position of the navbar
+let sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function checkSticky() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("header_sticky");
+  } else {
+    header.classList.remove("header_sticky");
+  }
+}
+
+// preloader
+const preloader = document.querySelector('.preloader');
+bodyElem.style.overflow = 'hidden';
+function hidePreloader(){
+  preloader.classList.add('preloader_hidden');
+  bodyElem.style.overflow = 'auto';
+}
+
+setTimeout(hidePreloader, 4000);
+
+// when click menu elem menu will close
+const menuLinks = document.querySelectorAll('.menu__link');
+menuLinks.forEach(elem => {
+  elem.addEventListener('click', function(){
+    menu.classList.remove('menu_active');
+    menuBtn.classList.remove('header__menu-btn_active');
+  });
+});
